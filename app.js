f2e982714c269ce2be2373b3117b36a3856795b5
Vexer79 <rokytskyi.i@gmail.com>
2024-04-01 03:22:22 +0300

    feat: add polinom lagrandj algorithm

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const express = require("express");
 const app = express();
@@ -6,6 +7,7 @@ const port = process.env.PORT || 3001;
 
 const userRoutes = require("./routes/user");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(userRoutes);
