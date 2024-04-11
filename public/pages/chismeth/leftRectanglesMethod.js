e3442b9a0ed08f202356781fda40888e23c3376b
mental-disfunctions <ultramegasupernice@gmail.com>
2024-04-11 18:54:17 +0300

    123

--- public/pages/chismeth/leftRectanglesMethod.js
+++ public/pages/chismeth/leftRectanglesMethod.js
@@ -1,9 +1,10 @@
+document.getElementById("startbtn").addEventListener("click", getValue);
 function getValue(){
     const func = document.getElementById("func").value;
     const n = document.getElementById("n").value;
     const a = document.getElementById("a").value;
     const b = document.getElementById("b").value;
-    
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
@@ -19,8 +20,10 @@ function getValue(){
         redirect: "follow"
     };
 
+    if(func && n && a && b){
 fetch("http://localhost:3001/integral/leftRectanglesMethod", requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
+}
 }
