99b6fb12db0b699c220a34d17dc726c07b40cedf
Vexer79 <rokytskyi.i@gmail.com>
2024-04-11 15:49:34 +0300

    feat: add get routes for left, right and center rectangles methods

--- routes/user.js
+++ routes/user.js
@@ -53,6 +53,16 @@ router.get("/polinom-lagrandg", (req, res) => {
     res.type(".html").sendFile(path.join(__dirname, "..", "public", "polinom-lagrandg.html"));
 });
 
+router.get("/integral/leftRectanglesMethod", (req, res) => {
+    res.type(".html").sendFile(path.join(__dirname, "..", "public", "pages", "leftRectanglesMethod.html"));
+});
+router.get("/integral/rightRectanglesMethod", (req, res) => {
+    res.type(".html").sendFile(path.join(__dirname, "..", "public", "pages", "rightRectanglesMethod.html"));
+});
+router.get("/integral/centerRectanglesMethod", (req, res) => {
+    res.type(".html").sendFile(path.join(__dirname, "..", "public", "pages", "centerRectanglesMethod.html"));
+});
+
 router.get("/", (req, res) => {
     res.type(".html").sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
