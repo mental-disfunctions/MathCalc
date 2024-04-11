63074a8b16f9f722c891341fae86ea09a748e39b
Vexer79 <rokytskyi.i@gmail.com>
2024-04-11 15:40:58 +0300

    feat: add post route to leftRectanglesMethod

---
+++ routes/algorithms.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const algorithms = require("../algorithms/algorithms");
+
+router.post("/integral/leftRectanglesMethod", (req, res) => {
+    res.json(
+        algorithms.Integral.leftRectanglesMethod(
+            new Function("return " + req.body.func)(),
+            Number(req.body.n),
+            JSON.parse(req.body.range)
+        )
+    );
+});
+
+module.exports = router;
