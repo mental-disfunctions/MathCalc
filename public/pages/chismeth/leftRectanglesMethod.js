46ce9e774a78e653d699a1f4e1d75870282a0327
mental-disfunctions <ultramegasupernice@gmail.com>
2024-04-11 18:37:17 +0300

    s

---
+++ public/pages/chismeth/leftRectanglesMethod.js
@@ -0,0 +1,26 @@
+function getValue(){
+    const func = document.getElementById("func").value;
+    const n = document.getElementById("n").value;
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("func", func);
+    urlencoded.append("n", n);
+    urlencoded.append("range", `[${a}, ${b}]`);
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: "follow"
+    };
+
+fetch("http://localhost:3001/integral/leftRectanglesMethod", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+}
